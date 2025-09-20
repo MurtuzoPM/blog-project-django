409df480d27a9c831d8307bfc5bbc5adb7b39074
Murtuzo <mmamadziyoev56@gmail.com>
2025-09-20 18:39:25 +0600

    first commit

---
+++ blog/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+def home(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+            return redirect('post_detail', pk=new_post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect('home')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # авто-вход после регистрации
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
---
+++ blog_project/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from blog import views as blog_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),   # блог
+    path('accounts/', include('django.contrib.auth.urls')),  # login/logout
+    path('signup/', blog_views.signup, name='signup'),       # регистрация
+]
